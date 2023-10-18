a2b5d50b6f436742c3502f9397203558dc9c3d9b
anishashruti <anishashruti.at@gmail.com>
2023-10-18 22:46:12 +0530

    created DB

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = f"mongodb+srv://admin:<pass>@clusterwhatsappbot.mlngtmk.mongodb.net/?retryWrites=true&w=majority"
+uri = f"mongodb+srv://admin:<pass>>@clusterwhatsappbot.mlngtmk.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
+
+dictionary_collection = client["MY_DB"]["dictionary"]
+
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
